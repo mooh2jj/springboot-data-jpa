5622b37d4186917972ab6fada7b169426135fa6c
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-04-09 01:26:39 +0900

    공통 인터페이스 테스트 적용

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -31,5 +32,35 @@ class MemberRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member);   // 같은 트랜잭션 내에는 영속성 컨텍스트안에서 똑같애!
+    }
+
+    // 순수 JPA 레포지토리 메서드도 다 구현이 됨을 확인함
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // 단건 조회 테스트
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+//        findMember1.setUsername("!!!!");      // 변경 감지 = 더티체킹
+
+        // 리스트 조회 테스트
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        // 카운트 검증
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        // 삭제 검증
+        memberRepository.delete(member1);
+        long deletedcount = memberRepository.count();
+        assertThat(deletedcount).isEqualTo(1);
     }
 }
